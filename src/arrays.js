7e89234206b1f6be5c57557d964e020448c9b241
David Requeno <davidrequeno52@gmail.com>
2023-08-05 22:18:41 -0600

    tuplas

--- src/arrays.js
+++ src/arrays.js
@@ -2,3 +2,7 @@
 //Arrays
 const languages = [];
 languages.push('Python');
+//Tuplas
+//Las tuplas en TypeScript son un tipo de datos que permiten representar una colección ordenada y fija de elementos, donde cada elemento puede tener un tipo diferente. A diferencia de los arrays, las tuplas tienen una longitud fija y los tipos de sus elementos están definidos en una secuencia específica.
+let persona;
+persona = ['David', 30, true];
--- src/arrays.ts
+++ src/arrays.ts
@@ -2,3 +2,10 @@
 const languages:(string | number)[] = []
 
 languages.push('Python')
+
+//Tuplas
+//Las tuplas en TypeScript son un tipo de datos que permiten representar una colección ordenada y fija de elementos, donde cada elemento puede tener un tipo diferente. A diferencia de los arrays, las tuplas tienen una longitud fija y los tipos de sus elementos están definidos en una secuencia específica.
+ 
+let persona: [string, number, boolean];
+persona = ['David', 30, true];
+
